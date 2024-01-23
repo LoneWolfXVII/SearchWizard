a9481e6035f9a78ed479b637dc3f2b310de2861a
RaunakMK <raunak@mrikal.com>
2024-01-23 19:20:56 +0530

    small bugs are done

--- react-ui/src/features/homescreen/PlayGround.js
+++ react-ui/src/features/homescreen/PlayGround.js
@@ -1,28 +1,14 @@
 import React from "react";
-import DotSvg from "./Waitlist_Assets/dot.svg";
+
 import KnowledgeGraph from "./KnowledgeGraph";
 import WebView from "./WebView";
-import ArrowSvg from "./Waitlist_Assets/arow.svg";
 
 const PlayGround = () => {
   return (
     <>
       <div className="w-full px-10 bg-[#000101] py-28">
         {/* Big button */}
-        <div className="flex justify-between Big_button">
-          <img src={DotSvg} alt="dot" width={90} className="mx-3" />
-          <div className="mt-20 Button_Heading">
-            <h1>Transform your business</h1>
-            <p>Launching soon, stay tuned for reveal!</p>
-            <button className="flex items-center justify-center w-64 p-2 my-3 space-x-3 text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
-              {" "}
-              <span>Join the Waitlist </span>
-              <img src={ArrowSvg} alt="Arrow" className="" />
-            </button>
-          </div>
 
-          <img src={DotSvg} alt="dot" width={90} className="mx-3" />
-        </div>
         <br />
         <br />
         <br />
--- react-ui/src/features/homescreen/Waitlist.js
+++ react-ui/src/features/homescreen/Waitlist.js
@@ -6,21 +6,42 @@ import PatternSvg from "./Waitlist_Assets/pattern.svg";
 import ChecklistSvg from "./Waitlist_Assets/Checklist.svg";
 import GroupSvg from "./Waitlist_Assets/Group.svg";
 import { useNavigate } from "react-router-dom";
+import DotSvg from "./Waitlist_Assets/dot.svg";
 
 const Waitlist = () => {
   const navigate = useNavigate();
   return (
     <div className="Waitlist_page bg-[#000101]">
       <section
-        className="px-20 py-10"
+        className="px-20 py-10 relative my-cool-item"
         style={{
           backgroundColor: "#000101",
           backgroundImage: `url('/background-gif.gif')`,
           objectFit: "cover",
           objectPosition: "center",
+          height: "100vh",
+          width: "100vw",
+          backgroundSize: "cover",
         }}
       >
-        <nav className="flex justify-between ">
+        {/* Black sheet with gradient overlay */}
+        {/* <div
+          className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-black via-grey to-transparent opacity-70"
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+        ></div>
+        <div
+          className="absolute  left-0 right-0 bottom-0 bg-gradient-to-b from-black via-grey to-transparent opacity-10"
+          style={{
+            height: "60%",
+            width: "100%",
+          }}
+        ></div> */}
+
+        {/* Content should be placed after the overlay to ensure it's on top */}
+        <nav className="flex justify-between z-10 relative">
           {/* Logo button */}
           <button className="text-lg font-bold text-white">Irame.ai</button>
 
@@ -28,23 +49,22 @@ const Waitlist = () => {
           <button className="Join_Waitlist_button">Join Waitlist</button>
         </nav>
 
-        <div className="pt-6 Hero_text">
+        <div className="pt-6 Hero_text z-10 relative">
           <h1>
             AI that excels in understanding
             <br />
             your business dynamics!
           </h1>
           <p>
-            IRAME is an advanced AI-focused semantic framework, powered by Knowledge Vector Graph and LLMs. It excels in knowledge processing and forecasting
-            enterprise needs, ensuring high accuracy and efficiency.
+            IRAME is an advanced AI-focused semantic framework, powered by Knowledge Vector Graph and LLMs. It excels in knowledge processing and forecasting enterprise
+            needs, ensuring high accuracy and efficiency.
           </p>
         </div>
 
         {/* Button for Join Waitlist and Learn More */}
-        <div className="flex justify-center gap-4 mt-10">
+        <div className="flex justify-center gap-4 mt-10 z-10 relative">
           <button className="flex items-center justify-center w-64 p-3 space-x-3 text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
             <span>Join the Waitlist </span>
-            <img src={ArrowSvg} alt="Arrow" className="" />
           </button>
           <button
             className="w-48 p-3 text-white border-2 border-white rounded-full hover:bg-blue-700"
@@ -124,6 +144,21 @@ const Waitlist = () => {
             </tr>
           </tbody>
         </table>
+        <div className="flex justify-between Big_button">
+          <img src={DotSvg} alt="dot" width={90} className="mx-3" />
+          <div className="mt-20 Button_Heading">
+            <h1>Transform your business</h1>
+            <p>Launching soon, stay tuned for reveal!</p>
+            <button className="flex items-center justify-center w-64 p-2 my-3 space-x-3 text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
+              {" "}
+              <span>Join the Waitlist </span>
+              <img src={ArrowSvg} alt="Arrow" className="" />
+            </button>
+          </div>
+
+          <img src={DotSvg} alt="dot" width={90} className="mx-3" />
+        </div>
+        <div className="h-40" />
       </section>
     </div>
   );
