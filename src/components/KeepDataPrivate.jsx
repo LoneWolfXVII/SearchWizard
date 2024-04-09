e8093cae22cdda2522fb522e29ca4eda72d7b87c
LoneWolfXVII <nischal.ngupta11@gmail.com>
2024-04-09 08:04:03 +0530

    responsive;

--- src/components/KeepDataPrivate.jsx
+++ src/components/KeepDataPrivate.jsx
@@ -11,7 +11,7 @@ const KeepDataPrivate = () => {
 							className=" w-full tPro:h-[500px]  h-auto object-contain"
 						/>
 					</div>
-					<div className="tPro:order-1 order-2 tPro:col-span-1 col-span-2 flex flex-col items-center content my-auto justify-start">
+					<div className="tPro:order-1 order-2 tPro:col-span-1 col-span-2 flex flex-col items-center content my-auto justify-start lpro:px-10 tPro:mt-12 mt-10 mx-10">
 						<h2 className="w-full">
 							Keep your Data
 							<span className="block">Private</span>
